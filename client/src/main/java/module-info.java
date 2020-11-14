f4d7bcb31e9fd3f6a22da3c1edcb0adb36cb24d1
Siarhei Audzeichyk <xlebysheklol@gmail.com>
2020-11-15 00:58:11 +0300

    add maven dependencies

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.xledbd {
     requires javafx.controls;
     requires javafx.fxml;
+    requires gson;
 
-    opens com.xledbd to javafx.fxml;
+    opens com.xledbd to javafx.fxml, gson;
     exports com.xledbd;
 }
