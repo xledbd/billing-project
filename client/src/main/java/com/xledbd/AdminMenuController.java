6d78a469cb64a39e6c0d2a01ec4415938e1af381
Siarhei Audzeichyk <xlebysheklol@gmail.com>
2020-12-05 22:42:38 +0300

    Add basic AdminMenu and ClientMenu forms

--- client/src/main/java/com/xledbd/AdminMenuController.java
+++ client/src/main/java/com/xledbd/AdminMenuController.java
@@ -1,4 +1,38 @@
 package com.xledbd;
 
-public class AdminMenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+	@FXML private Label infoLabel;
+	@FXML private Label loginLabel;
+
+	public void changeSceneToUserMenu(ActionEvent event) throws IOException {
+		Stage window = (Stage)App.scene.getWindow();
+		window.setTitle("Управление пользователями");
+		App.setRoot("userMenu");
+		window.setScene(App.scene);
+		window.show();
+	}
+
+	public void changeSceneToLoginView(ActionEvent event) throws IOException {
+		Stage window = (Stage)App.scene.getWindow();
+		window.setTitle("Добро пожаловать");
+		App.setRoot("loginMenu");
+		window.setScene(App.scene);
+		window.show();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		loginLabel.setText(App.user.getLogin());
+		infoLabel.setText("Администратор:");
+	}
 }
